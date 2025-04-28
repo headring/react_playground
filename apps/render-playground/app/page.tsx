1edb2969a175de376044c6a70c8c895a3e0b0816
SangHyun Kim <myhakiki@gmail.com>
2025-04-28 14:49:56 +0900

    feat: 스토어 열기 테스트

--- apps/render-playground/app/page.tsx
+++ apps/render-playground/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { memo, useState } from "react";
-
+import { useRouter } from 'next/navigation'
 const InnerComponent = (props: any) => {
   const { count } = props;
   if (count) {
@@ -22,6 +22,7 @@ const MemoComponent2 = memo((props: { memoCount: number }) => {
 function Home(props: any) {
   const [count, setCount] = useState(0);
   const [memoCount, setMemoCount] = useState(0);
+  const router = useRouter();
 
   return (
     <div>
@@ -32,6 +33,12 @@ function Home(props: any) {
           id="rasied-image-file"
           name="qnaImage"
        /> 
+      <div><button onClick=()=> { 
+        router.push("https://apps.apple.com/kr/app/%EA%B8%88%EB%B0%A9%EA%B8%88%EB%B0%A9/id1575200660")
+      }>React 이동하기</button></div>
+    <div><button onClick=()=> { 
+        window.open("https://apps.apple.com/kr/app/%EA%B8%88%EB%B0%A9%EA%B8%88%EB%B0%A9/id1575200660")
+      }>새 창 열기 이동하기</button></div>
       <ul>
         devtools의 render는 Render Phase를 의미. 자신의 상태가 변경되거나,
         props가 변경되거나, 부모가 변경되어서 하위값들이 render phase 진입

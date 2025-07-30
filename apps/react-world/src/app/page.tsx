db4d398019d19441b95865d2ccf006848db34214
SangHyun Kim <myhakiki@gmail.com>
2025-07-30 00:52:51 +0000

    refactor: 불필요 컴포넌트 제거

--- apps/react-world/src/app/page.tsx
+++ apps/react-world/src/app/page.tsx
@@ -1,5 +1,3 @@
-import { TestSuspension } from "./_components/TestSuspension";
-
 export const fetchAritcle = () => {
   return fetch("https://api.realworld.build/api/articles?limit=10&offset=0");
 };
@@ -11,7 +9,7 @@ export default function Page() {
     <h1 className="text-3xl font-bold underline">
       <div>Real World - Conduit</div>
       <div>Home, sign, sign up</div>
-      <TestSuspension testData="This is a test message" />
+
     </h1>
   );
 }

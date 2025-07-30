79eac03e4d32e18716aec8646650ee7951727d22
SangHyun Kim <myhakiki@gmail.com>
2025-07-30 07:56:18 +0000

    chore: next.config.js 생성 후 이미지 URL 기재

---
+++ apps/react-world/next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      new URL(
+        "https://raw.githubusercontent.com/gothinkster/node-express-realworld-example-app/refs/heads/master/src/**",
+      ),
+    ],
+  },
+};
